533c4278e1f1644030c141e11e937026c20690fc
duslia2025 <dusliachrismelmbila@gmail.com>
2026-06-12 15:46:27 +0100

    Add files via upload

---
+++ SQLQuery18.sql
@@ -0,0 +1,7 @@
+/* =============================================
+   EC_IT143 - Fun With Triggers
+   Step 5: Test Result
+   ============================================= */
+
+-- Update a record and verify that the trigger
+-- correctly updates the ModifiedDate value.
